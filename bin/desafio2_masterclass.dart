8e8bedb63ec0a03ef2bc9153ae98f4313315de6d
Pedro Henrique Do N Siqueira <pedro.siqueira1999@hotmail.com>
2023-06-01 11:50:49 -0300

    sequencia fibonacci correta

--- bin/desafio2_masterclass.dart
+++ bin/desafio2_masterclass.dart
@@ -1,14 +1,40 @@
-//não está com função recursiva. :(
-//mas pelo menos consegui resolver
+//!feito com auxilio da internet.
 
-void main() {
-  int valor = 60;
-  List<int> valores = [];
-  
-  for (int i = 0; i < valor; i++) {
-    valores.add(i);
+void main(List<String> args) {
+  int valor = 0;
+  var fibonacciList = [];
+  for (var i = 0; i < valor; i++) {
+    fibonacciList.add(fibonacci(i));
   }
- 
-
-  print(valores.reduce((x, y) => x+y));
+  print(fibonacciList);
 }
+
+int fibonacci(int numero) {
+  if (numero <=1) {
+    return numero;
+  } else {
+    
+    return fibonacci(numero - 1) + fibonacci(numero - 2);
+  }
+}
+
+
+
+
+
+
+
+
+
+//!feito sem auxilio da internet
+// void main() {
+//   int valor = 3;
+//   List<int> valores = [];
+
+//   for (int i = 0; i < valor; i++) {
+//     valores.add(i);
+//   }
+//   print(valores);
+
+//   print(valores.reduce((x, y) => x + y));
+// }
